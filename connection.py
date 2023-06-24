dae98f5c77345e785cfb64de57758c8244ee7d1d
17abd12 <abdullahimran1712@example.com>
2023-06-24 15:32:11 +0500

    creating socket structure

--- connection.py
+++ connection.py
@@ -1,15 +1,16 @@
 import socket
 import os
-identifier = "<end of file>"
+identifier = "<END OF FILE>"
 arp_addresss = ("192.168.18.149",9000)
 s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 s.connect(arp_addresss)
 while True:
     command = s.recv(1024)
-    command = command.encode()
+    command = command.decode()
     print(command)
     if command == "STOP":
         s.close()
+        break
     message = "okay so let's get started again"
     message = message + identifier
     s.send(message.encode())
